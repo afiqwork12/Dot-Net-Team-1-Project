8879bba47a1b82117a2a055d6059f073e84e7d59
afiqster12 <afiqwork12@outlook.com>
2022-02-10 15:00:50 +0800

    added more models

---
+++ Actual Project/CarouselForBooksSolution/CarouselForBooksApplication/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarouselForBooksApplication.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<Genre> Genres { get; set; }
+        public string Description { get; set; }
+        public string Language { get; set; }
+        public double Cost { get; set; }
+        public string Picture { get; set; }
+        public bool IsSold { get; set; }
+        public DateTime DatePosted { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Actual Project/CarouselForBooksSolution/CarouselForBooksApplication/Models/BookGenre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarouselForBooksApplication.Models
+{
+    public class BookGenre
+    {
+        public int BookId { get; set; }
+        public int GenreId { get; set; }
+    }
+}
