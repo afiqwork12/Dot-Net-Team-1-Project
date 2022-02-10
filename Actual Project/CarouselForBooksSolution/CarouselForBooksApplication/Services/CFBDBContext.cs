ba2ef9f61a95697613478090a5d4a6d325bc6b20
afiqster12 <afiqwork12@outlook.com>
2022-02-10 14:33:15 +0800

    added a project

---
+++ Actual Project/CarouselForBooksSolution/CarouselForBooksApplication/Services/CFBDBContext.cs
@@ -0,0 +1,33 @@
+﻿using CarouselForBooksApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarouselForBooksApplication.Services
+{
+    public class CFBDBContext : DbContext
+    {
+        public CFBDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User()
+                {
+                    Id = 1,
+                    Username = "john123",
+                    Password = "P@ssw0rd123",
+                    Name = "John Wick",
+                    Dob = DateTime.Now,
+                    Email = "john123@gmail.com"
+                }
+            );
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Actual Project/CarouselForBooksSolution/CarouselForBooksApplication/Services/UserEFRepo.cs
@@ -0,0 +1,41 @@
+﻿using CarouselForBooksApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarouselForBooksApplication.Services
+{
+    public class UserEFRepo : IRepo<int, User>
+    {
+        private readonly CFBDBContext _context;
+        public UserEFRepo(CFBDBContext context)
+        {
+            _context = context;
+        }
+        public bool Add(User t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int k)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetT(int k)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(User t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
